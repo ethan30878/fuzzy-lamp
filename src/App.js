92e5f9c30487c8d78b3b6b7272139dde09e67ddf
ethan30878 <ethan30878@gmail.com>
2023-10-24 11:21:04 -0400

    3rd Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>"CS 230L</h1>
+      <h1>CS 230L</h1>
       <h2>Section - 02</h2>
       <p>WVU ID: 800390863</p>
       <p>Hi I am Ethan</p>
