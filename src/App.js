3e927ab9b3d22ec8883863d70329c1043f345005
ethan30878 <ethan30878@gmail.com>
2023-10-30 11:17:39 -0400

    Test Commit 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
@@ -36,6 +38,19 @@ function App() {
           </Navbar.Collapse>
         </Container>
         </Navbar>
+      </div>
+      <div>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+        </Card>
       </div>
     </div>
   );
