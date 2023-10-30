37420dc58b40f70876ba98d4d2b74b228f7c0391
ethan30878 <ethan30878@gmail.com>
2023-10-30 12:26:27 -0400

    6th commit

--- src/App.js
+++ src/App.js
@@ -39,44 +39,46 @@ function App() {
         </Container>
         </Navbar>
       </div>
-      <div style="display:flex; flex-direction: row;">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="./src/funny.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-        </Card>
-      </div>
-      <div style="display:flex; flex-direction: row;">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="./src/funny.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-        </Card>
-      </div>
-      <div style="display:flex; flex-direction: row;">
-        <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="./src/funny.jpg" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-        </Card>
+      <div class="d-flex flex-row">
+        <div class="card mx-auto">
+          <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="./src/funny.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+          </Card>
+        </div>
+        <div class="card mx-auto">
+          <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="./src/funny.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+          </Card>
+        </div>
+        <div class="card mx-auto">
+          <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="./src/funny.jpg" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+          </Card>
+        </div>
       </div>
     </div>
   );
