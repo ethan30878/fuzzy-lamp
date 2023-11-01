70a8d7c59cc5d1a6d7d0a0b940aff693311264b9
ethan30878 <ethan30878@gmail.com>
2023-10-31 20:10:08 -0400

    Lab7 Final Commit

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-function Contact(props) {
+function Contact() {
     return(
         <div className="Contact">
-            <h1>Contact here</h1>
+            <h1>Contact Here</h1>
+            <form>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We <i>might</i> share your email with someone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Message</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 }
